5069fbea488d9ec55fe1f9d526e9a953905186d1
Jan Kędzierski <jkedzierski087@gmail.com>
2023-12-14 01:58:31 +0100

    finish

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,38 @@
-/** INSERT CUSTOM JS HERE **/
-export default function () {}
+function toggleOverlay() {
+  const overlay = document.getElementById('overlay');
+  const whiteNavbar = document.getElementById('whiteNavbar');
+  const barsButton = document.getElementById('bars');
+  const mobileNavLinks = document.getElementById('mobileNavLinks');
+  const crossImage = document.getElementById('crossImage');
+  const rightMobileSide = document.getElementById('rightMoblieSide');
+
+  overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
+  whiteNavbar.style.display = whiteNavbar.style.display === 'block' ? 'none' : 'block';
+  crossImage.style.display = whiteNavbar.style.display === 'block' ? 'block' : 'none';
+
+  while (mobileNavLinks.firstChild) {
+    whiteNavbar.appendChild(mobileNavLinks.firstChild);
+  }
+  mobileNavLinks.style.display = 'none';
+
+  while (rightMobileSide.firstChild) {
+    whiteNavbar.appendChild(rightMobileSide.firstChild);
+  }
+  rightMobileSide.style.display = 'none';
+}
+
+function closeWhiteNavbar() {
+  const overlay = document.getElementById('overlay');
+  const whiteNavbar = document.getElementById('whiteNavbar');
+  const crossImage = document.getElementById('crossImage');
+
+  overlay.style.display = 'none';
+  whiteNavbar.style.display = 'none';
+  crossImage.style.display = 'none';
+}
 
 document.addEventListener('DOMContentLoaded', function () {
+  //yt film
   const watchVideoText = document.getElementById('watchVideoText');
   const videoOverlay = document.getElementById('videoOverlay');
   const closeButton = document.getElementById('closeButton');
@@ -20,4 +51,9 @@ document.addEventListener('DOMContentLoaded', function () {
     youtubeVideo.src = '';
     videoOverlay.style.display = 'none';
   });
+  // koniec yt
+
+  //nawigacja
+  const barsButton = document.getElementById('bars');
+  barsButton.addEventListener('click', toggleOverlay);
 });
